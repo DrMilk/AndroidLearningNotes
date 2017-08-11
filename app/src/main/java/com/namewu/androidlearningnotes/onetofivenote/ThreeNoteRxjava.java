a039ddb9b6cfad5854e1e1a9f8603e4730b0ff45
Administrator <18249028972@163.com>
2017-08-11 21:01:55 +0800

    Rxjava

--- app/src/main/java/com/namewu/androidlearningnotes/onetofivenote/ThreeNoteRxjava.java
+++ app/src/main/java/com/namewu/androidlearningnotes/onetofivenote/ThreeNoteRxjava.java
@@ -1,8 +1,15 @@
 package com.namewu.androidlearningnotes.onetofivenote;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by Administrator on 2017/8/10.
  */
 
-public class ThreeNoteRxjava {
+public class ThreeNoteRxjava extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
